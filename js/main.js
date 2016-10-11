20435df5db1d002ffaea5e8bc40ea69c4ded3b3c
CJC9 <engnatruve@gmail.com>
2016-10-10 21:43:49 -0500

    Ejercicio arrays

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@ var pageLoaded = function(){
   console.log("Página Cargada");
 };
 document.addEventListener("DOMContentLoaded" , function(){
-  var list_li = document.querySelectorAll(".list_item");//devuelve los elementos dentro de un array
+  var hash = {
+    key: "value",
+    key1: "value",
+    key2: "value",
+    key3: "value",
+    key4: "value",
+    key5: "value",
+    key6: "value",
+    key7: "value",
+    key8: "value",
+  };
+
+  console.log(hash);
+  /*var list_li = document.querySelectorAll(".list_item");//devuelve los elementos dentro de un array
   list_li = Array.from(list_li);
   console.log("Primer For each");
   for(var li_pos in list_li){
@@ -19,6 +32,17 @@ document.addEventListener("DOMContentLoaded" , function(){
     //console.log(array.shift());
     //console.log(array.pop());
 
-  });
+  });*/
+
+/*var carro = {
+  ruedas : ["fr" , "fl" , "tr" , "tl" ],
+  representacion_html : null,
+};
+
+carro.representacion_html = document.createElement("div");
+document.appendChild(carro.representacion_html);
+carro.addEventListener("click" , function(){
+
+});*/
 
 }); //Escucha que responde a un evento
